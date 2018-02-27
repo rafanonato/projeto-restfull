5717dfcdc064588e9d954c7ff221296a93b32b99
rafanonato <rf.nonato@gmail.com>
2018-02-27 15:22:01 -0300

    Add layer of persistence

--- src/main/java/com/projeto/api/MeuProjetoApplication.java
+++ src/main/java/com/projeto/api/MeuProjetoApplication.java
@@ -1,12 +1,26 @@
 package com.projeto.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MeuProjetoApplication {
 
+	@Value("${paginacao.qtda_por_pag}")
+	private int qtdaPage;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MeuProjetoApplication.class, args);
+
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner() {
+		return args -> {
+			System.out.println(this.qtdaPage);
+		};
 	}
 }
